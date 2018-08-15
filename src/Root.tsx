3c38541b89e09f18397331d1e863119c5eed61ad
Divyansh Mahajan <divyanshmahajan.24@gmail.com>
2018-08-15 23:56:45 +0530

    refactor(root): add new information

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   public render() {
     return (
       <View style={styles.container}>
+        <Text>This is a typescript react-native boilerplate.</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Text>Open up App.js to start working on your app!</Text>
@@ -15,9 +16,9 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    alignItems: "center",
+    backgroundColor: "#fff",
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center"
   }
 });
